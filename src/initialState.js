04b79312da7e8739f0382af03251ec3e29328724
Juan Rios <mrdaniel19996@gmail.com>
2020-09-18 18:38:30 -0500

    [UPDATE] Travis yml added

--- src/initialState.js
+++ src/initialState.js
@@ -4,7 +4,7 @@ export default {
     {
       'id': '1',
       'image': 'https://arepa.s3.amazonaws.com/camiseta.png',
-      'title': 'Camiseta',
+      'title':'Polera',
       'price': 25,
       'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     },
